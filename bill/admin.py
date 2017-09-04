52edd1f231921c0e31d9377ed377b23b296da844
jijoxvr <jijoxvr315@gmail.com>
2017-09-04 17:37:35 +0530

    Design model

--- bill/admin.py
+++ bill/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bill
+from vendor.models import Vendor
+
+admin.site.register(Bill)
+admin.site.register(Vendor)
 
 # Register your models here.
--- django_rest_sample/settings.py
+++ django_rest_sample/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bill',
+    'vendor'
 ]
 
 MIDDLEWARE_CLASSES = [
