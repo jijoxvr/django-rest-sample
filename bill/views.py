94761ff76a3b959ba639e32270edccb459f4dc58
jijoxvr <jijoxvr315@gmail.com>
2017-09-05 10:33:06 +0530

    API using model viewset

--- bill/views.py
+++ bill/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Bill
+from .serializer import BillModelSerializer
+
+class BillModelViewSet(viewsets.ModelViewSet):
+    queryset = Bill.objects.all()
+    serializer_class = BillModelSerializer
 
 # Create your views here.
